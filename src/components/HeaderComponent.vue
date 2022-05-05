cbfe96f9ff8369861a1247fa2c6c4060c1233bc3
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-05 15:45:30 +0200

    add header component section

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <header>
+        <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+
+.logo{
+    width: 50px;
+    margin: 1rem;
+}
+
+</style>
