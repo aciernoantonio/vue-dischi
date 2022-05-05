6df80e9ad8608ce2374db274cd8a7dc859d882ad
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-05 15:15:52 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <SiteHeader/>
+    <SiteMain/>
+
+  </div>
+</template>
+
+<script>
+import SiteHeader from "@/components/HeaderComponent.vue"
+import SiteMain from "@/components/MainComponent.vue"
+
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader,
+    SiteMain
+  }
+}
+</script>
+
+<style lang="scss">
+
+@use "@/assets/scss/style.scss";
+
+</style>
